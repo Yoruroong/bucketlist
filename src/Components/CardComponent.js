e8eb13ae20c82e32befbda0a7334238ecb187058
Yoruroong <tuntun4132@naver.com>
2022-01-15 08:51:17 +0900

    successdetailmodal

--- src/Components/CardComponent.js
+++ src/Components/CardComponent.js
@@ -1,18 +1,52 @@
-import {Card} from 'react-bootstrap'
+import {Card, Button, Modal} from 'react-bootstrap'
+import React from 'react'
 
 export default function CardComponent({note}) {
+    const [modalShow, setModalShow] = React.useState(false);
     return (
-        <Card style={note.successdetails ? { cursor: "pointer" } : { cursor: "default" }} className={note.successdetails ? "shadow hoverbackground": "shadow"}>
-          <Card.Img variant="top" src={note.image ? "images/" + note.image : ""} />
-          <Card.Body>
-            <Card.Title>{note.title}</Card.Title>
-            <Card.Text>
-              {note.details}
-            </Card.Text>
-            <Card.Footer>
-                <small className="text-muted">{note.success == 1 ? `완료됨: ${note.date}` : "미완료"}</small>
-            </Card.Footer>
-          </Card.Body>
+        <Card style={note.successdetails ? { cursor: "pointer" } : { cursor: "default" }} className={note.successdetails ? "shadow hoverbackground": "shadow"} onClick={() => setModalShow(true)}>
+            <Card.Img variant="top" src={note.image ? "images/" + note.image : ""} />
+            <Card.Body>
+                <Card.Title>{note.title}</Card.Title>
+                <Card.Text>
+                    {note.details}
+                </Card.Text>
+                <Card.Footer>
+                    <small className="text-muted">{note.success == 1 ? `완료됨: ${note.date}` : "미완료"}</small>
+                </Card.Footer>
+            </Card.Body>
+            <NoteModal
+                show={modalShow}
+                note={note}
+                onHide={() => setModalShow(false)}
+            />
         </Card>
     )
 }
+
+function NoteModal(props) {
+    return (
+      <Modal
+        {...props}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header>
+          <Modal.Title id="contained-modal-title-vcenter">
+            {props.note.title}
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+            {props.note.successimage ? <img src={"images/" + props.note.successimage} className="img-fluid"></img> : ""}
+          <p style={{ marginTop: '17px' }}>
+            {props.note.successdetails}
+          </p>
+        </Modal.Body>
+        <Modal.Footer>
+            
+            <Button onClick={() => window.location.href="/"}>Close</Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  }
