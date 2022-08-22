9df5dafbec2296ed6eab08c77634fc48e9658506
Yoruroong <tuntun4132@naver.com>
2022-08-23 00:29:28 +0900

    fix: 10 successdetails

--- src/App.js
+++ src/App.js
@@ -121,7 +121,7 @@ function App() {
             "success": "1",
             "date": "2022.02.23",
             "image": "",
-            "successdetails": "원래는 계획에 없었느데 해운대 해수욕장 가서 마린시티에 가 본게 됨.",
+            "successdetails": "원래는 계획에 없었는데 해운대 해수욕장 가서 마린시티에 가 본게 됨.",
             "successimage": "",
             "id": 10
           },
