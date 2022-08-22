efeb827b572f569bc170a8c27b63d6c134b2a063
Yoruroong <tuntun4132@naver.com>
2022-08-23 01:13:18 +0900

    asdf

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -18,46 +18,46 @@ export default function Navbarin() {
     }
 
     return (
-    <Navbar bg="light" expand="lg" fixed="top">
-      <Container fluid>
-        <Navbar.Brand href="#">Bucketlist</Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: '100px' }}
-            navbarScroll
-          >
+        <Navbar bg="light" expand="lg" fixed="top">
+            <Container fluid>
+            <Navbar.Brand href="#">Bucketlist</Navbar.Brand>
+            <Navbar.Toggle aria-controls="navbarScroll" />
+            <Navbar.Collapse id="navbarScroll">
+            <Nav
+                className="me-auto my-2 my-lg-0"
+                style={{ maxHeight: '100px' }}
+                navbarScroll
+            >
             <Nav.Link href="/" onClick={makeitloading}>{loading?"":"메인"}<Spinner style={{ width: loading? "": "0px", visibility: loading ? "visible":"hidden" }} animation="grow" size="sm"/></Nav.Link>
             <Nav.Link href="/success">달성</Nav.Link>
             
             <NavDropdown title="Link" id="navbarScrollingDropdown">
-              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action4">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action5">
-                Something else here
-              </NavDropdown.Item>
-            </NavDropdown>
-            <Nav.Link href="#" disabled>
-              Link
-            </Nav.Link>
-          </Nav>
-          <Form className="d-flex">
-            <Form.Control
-              type="search"
-              placeholder="place ID of element"
-              className="me-2"
-              aria-label="Search"
-              onChange={handleChange}
-                value={message}
-            />
-            <Button variant="outline-success" onClick={gotoElement}>Go</Button>
-          </Form>
-        </Navbar.Collapse>
-      </Container>
+                <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action4">
+                    Another action
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action5">
+                    Something else here
+                </NavDropdown.Item>
+                </NavDropdown>
+                <Nav.Link href="#" disabled>
+                    Link
+                </Nav.Link>
+            </Nav>
+            <Form className="d-flex">
+                <Form.Control
+                    type="search"
+                    placeholder="place ID of element"
+                    className="me-2"
+                    aria-label="Search"
+                    onChange={handleChange}
+                    value={message}
+                />
+                <Button variant="outline-success" onClick={gotoElement}>Go</Button>
+            </Form>
+            </Navbar.Collapse>
+        </Container>
     </Navbar>
   );
 }
