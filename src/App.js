b006072d8ebed35b91078f15294609c4aa560a94
Yoruroong <tuntun4132@naver.com>
2023-01-22 22:16:23 +0900

    add loader

--- src/App.js
+++ src/App.js
@@ -5,17 +5,21 @@ import Stats from "./Components/Stats";
 import Success from "./Pages/Success";
 import Navbar from "./Components/Navbar"
 import './App.css';
+import {ClipLoader, DotLoader, MoonLoader, RingLoader, SyncLoader} from "react-spinners";
 
 export default function App() {
   const [notes, setNotes] = useState([]);
+  const [isLoading, setisLoading] = useState([]);
   // https://jinh.kr/bucket/
 
   useEffect(() => {
+    setisLoading(true)
     fetch('https://bucketapi.yeonw.me/getall')
       .then(res => res.json())
       .then(data => {
         console.log(data)
         setNotes(data)
+        setisLoading(false)
       })
     document.title=new Date().getMonth() == 9 && new Date().getDate() == 9 ? "연우가 만든 버킷리스트" : "Bucket List by Yeonwoo"
   }, [])
@@ -24,11 +28,22 @@ export default function App() {
     <div className="App-header">
       <Navbar />
       <h1 style={{ marginTop: "100px" }}># {new Date().getMonth() == 9 && new Date().getDate() == 9 ? "버킷리스트" : "BUCKETLIST"}</h1>
-      <Stats notes={notes} />
+      {isLoading ? (<div><MoonLoader
+        color={"#5cdb95"}
+        loading={isLoading}
+        size={150}
+        aria-label="Loading Spinner"
+        data-testid="loader"
+      /></div>) : (
+        <div>
+        <Stats notes={notes} />
       <Routes>
         <Route path="/" element={<Card notes={notes}/>} />
         <Route path="/success" element={<Success notes={notes}/>} />
       </Routes>
+      </div>
+      )}
+      
       <footer>
         <p align="center" className="copy">© 2020 - {new Date().getFullYear()}, Yeonwoo Seo </p>
       </footer>
