8851a8cb24220c900a8c5e8d1322e666ea6cd5b0
Yeonwoo Seo <33586417+Yoruroong@users.noreply.github.com>
2025-06-08 21:54:09 +0900

    hide travel category

--- src/Pages/Success.js
+++ src/Pages/Success.js
@@ -6,7 +6,7 @@ export default function RCard({ notes }) {
     return (
       <>
         <div className="cards">
-          {["TOP", "Teenager", "여행", "음식", "사람", "소유", "교육", "기타"].map(ct => (
+          {["TOP", "Teenager", "음식", "사람", "소유", "교육", "기타"].map(ct => (
             <div>
               <h2 style={{ marginTop: '10px' }}>#{ct}</h2>
               <Row xs={1} md={4} className="g-4" style={{ alignContent: "left", marginRight: '0' }}>
